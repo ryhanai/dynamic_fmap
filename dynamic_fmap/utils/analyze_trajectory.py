07b02b528762bc710455c4feafccf80683ed0b56
Ryo Hanai <ryo.hanai@aist.go.jp>
2026-02-03 18:48:41 +0900

    plot the trajectories of object and interaction forces

--- dynamic_fmap/utils/analyze_trajectory.py
+++ dynamic_fmap/utils/analyze_trajectory.py
@@ -7,6 +7,7 @@ from importlib.resources import files
 import yaml
 from dataclasses import dataclass
 import tyro
+import matplotlib.pyplot as plt
 
 
 def get_trajectory_files():
@@ -57,6 +58,51 @@ def analyze_demos(demo_root: Path, trajectory_files: List[Path]):
     return np.concatenate(all_points, axis=0), np.concatenate(all_forces, axis=0)
 
 
+def foo(task: str, traj_id=0, demo_method: str = 'motionplanning'):
+    demo_file_path = Path('~').expanduser() / 'Downloads' / '250923' / task / demo_method
+    if demo_method == 'motionplanning':
+        demo_file_path /= 'trajectory.state+rgb+ts_force.pd_joint_pos.physx_cpu.h5'
+    elif demo_method == 'rl':
+        demo_file_path /= 'trajectory.state+rgb.pd_ee_delta_pose.physx_cpu.h5'    
+
+    with h5py.File(demo_file_path) as fl:
+        traj = list(fl.keys())[traj_id]
+        T = fl[f'{traj}/obs/sensor_data/force_camera/point_forces'].shape[0]
+        # return [fl[f'{traj}/obs/sensor_data/force_camera/point_forces'][t][:,3.6].sum(axis=0) for t in range(T)]
+        pf = [fl[f'{traj}/obs/sensor_data/force_camera/point_forces'][t][:, 3:6] for t in range(T)]
+        try:
+            peg_states = np.asarray(fl[f'{traj}/env_states/actors/peg_0'])
+        except KeyError:
+            peg_states = np.asarray(fl[f'{traj}/env_states/actors/peg'])
+
+        return np.linalg.norm(np.stack(pf), axis=-1).sum(axis=1), peg_states
+
+        # trajs = list(fl.keys())
+        # for traj in trajs:
+        #     force_values = get_force_values_from_trajectory(fl, traj)
+        #     print(f"Trajectory {traj} force values: min {force_values.min()}, max {force_values.max()}")    
+
+def plot_trajectories(time_series: List[np.ndarray], y_labels: List[str] = None, title: str = ""):
+    plt.figure()
+    for values, y_label in zip(time_series, y_labels):
+        plt.plot(values, label=y_label)
+
+    plt.title(title)
+    plt.xlabel("time step")
+    plt.ylabel("value")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def plot_force_and_peg_state(task: str, traj_id=0, demo_method: str = 'motionplanning'):
+    p = foo(task, traj_id, demo_method)
+    plot_trajectories(
+        [p[0]*1e-3, p[1][:,0], p[1][:,1], p[1][:,2]],
+        y_labels=['force', 'peg.pose.x', 'peg.pose.y', 'peg.pose.z'],
+        title=demo_method,
+        )
+
+
 @dataclass
 class Args:
     demo_root: Path  # root directory of demo data
@@ -65,6 +111,6 @@ def main(args: Args):
     demo_root = args.demo_root
     analyze_demos(demo_root, get_trajectory_files())
 
-if __name__ == '__main__':
-    args = tyro.cli(Args)
-    main(args)
+# if __name__ == '__main__':
+#     args = tyro.cli(Args)
+#     main(args)
